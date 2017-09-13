8f0b966c937f15cc014bc3a167838fb1728ecc86
godka <mythkast@hotmail.com>
2017-09-13 16:21:51 +0800

    delete webrtc remote video

--- static/js/index.js
+++ static/js/index.js
@@ -198,14 +198,14 @@ function sendMessage(message) {
 }
 
 function showSpinner() {
-	for (var i = 0; i < arguments.length; i++) {
+	for (var i = 0; i < arguments.length - 1; i++) {
 		arguments[i].poster = './img/transparent-1px.png';
 		arguments[i].style.background = 'center transparent url("./img/spinner.gif") no-repeat';
 	}
 }
 
 function hideSpinner() {
-	for (var i = 0; i < arguments.length; i++) {
+	for (var i = 0; i < arguments.length - 1; i++) {
 		arguments[i].src = '';
 		arguments[i].poster = './img/webrtc.png';
 		arguments[i].style.background = '';
